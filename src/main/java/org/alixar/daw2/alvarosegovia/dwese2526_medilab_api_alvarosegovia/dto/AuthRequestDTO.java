ef7d95b31e57972cf49058cb6517c1a7e38a0452
alvaro-segovia-ffw <segovia@future-forward.de>
2026-04-14 17:39:55 +0200

    Validar longitud mínima de contraseña en login

--- src/main/java/org/alixar/daw2/alvarosegovia/dwese2526_medilab_api_alvarosegovia/dto/AuthRequestDTO.java
+++ src/main/java/org/alixar/daw2/alvarosegovia/dwese2526_medilab_api_alvarosegovia/dto/AuthRequestDTO.java
@@ -2,6 +2,7 @@ package org.alixar.daw2.alvarosegovia.dwese2526_medilab_api_alvarosegovia.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,5 +13,6 @@ public class AuthRequestDTO {
     private String email;
 
     @NotBlank
+    @Size(min = 8, max = 72, message = "{msg.auth.password.size}")
     private String password;
 }
